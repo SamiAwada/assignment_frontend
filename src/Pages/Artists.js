82eb6b856efbfbeb5aa8df5a025b63dd241ec486
Sami Awada <samiawada30@gmail.com>
2022-04-13 04:39:27 +0000

    added debounce function to the search bar

--- src/Pages/Artists.js
+++ src/Pages/Artists.js
@@ -2,8 +2,17 @@ import React from 'react'
 import styles from "../Assets/scss/ArtistsPage/ArtistsPage.module.scss";
 import searchIcon from "../Assets/images/svg/searchIcon.svg";
 import Pagination from "@mui/material/Pagination";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Artists() {
+  const handleSearch = useDebouncedCallback(
+    // function
+    (value) => {
+      console.log(value);
+    },
+    // delay in ms
+    400
+  );
   return (
     <div className="container mt-3">
       <div className={"d-flex flex-column " + styles.artistsMainBody}>
