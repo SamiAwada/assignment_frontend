87da3019bcfc3bb667ce5ecc3dd0e77016642f87
Sami Awada <samiawada30@gmail.com>
2022-04-22 22:43:51 +0300

    added artists card component

--- src/Shared/components/Cards/Cards.js
+++ src/Shared/components/Cards/Cards.js
@@ -4,15 +4,13 @@ export default function Cards(props) {
   return (
     <>
       {props.cardsList.map((card, i) => (
-        <div className="card mb-2">
+        <div className="card mb-2" key={"artist"+i}>
           <div className="card-body">
-            <h5 className="card-title">{card.userName}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{card.Talent}</h6>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <span className="card-link">{card.phoneNumber}</span>
+            <h5 className="card-title">{card.name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Followers {card.followers}</h6>
+            <a href={""+card.artistURL} className="card-link">
+              Visit Profile
+            </a>
           </div>
         </div>
       ))}
